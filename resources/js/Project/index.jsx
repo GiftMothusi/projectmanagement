00519d84a71fa655703eecc564d6d1cf17799d25
Gift <giftgabanakgosi@gmail.com>
2024-07-22 19:55:33 +0200

    user controller + project route + tasks routes+ added to new routes to authenticated layout

---
+++ resources/js/Project/index.jsx
@@ -0,0 +1,12 @@
+import Authenticated from '@/Layouts/AuthenticatedLayout'
+import React from 'react'
+
+const Index = () => {
+  return (
+   <Authenticated>
+
+   </Authenticated>
+  )
+}
+
+export default Index
